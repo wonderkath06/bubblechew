9050b0e8fe7522e7d840aa8a6dfc27efde8f9f54
wonderkath06 <kathrin@heleva.no>
2025-01-28 10:44:35 +0100

    i tried

--- bubble gum shooter/Assets/Scripts/DamageFromEnemy.cs
+++ bubble gum shooter/Assets/Scripts/DamageFromEnemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DamageFromEnemy : MonoBehaviour
 {
-    public int damage = 2;
+    public int damage = 1;
     private Health playerHealth;
     // Start is called before the first frame update
     void Start()
